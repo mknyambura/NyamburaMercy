e390180e10e158b41ece078d9896c1a3888158bc
Mercy Nyambura Kariuki <kariuki.mercie@gmail.com>
2023-06-12 15:35:23 +0300

    create-react-app-with-vite

--- backend-sanity/sanity.cli.js
+++
@@ -1,8 +0,0 @@
-import {defineCliConfig} from 'sanity/cli'
-
-export default defineCliConfig({
-  api: {
-    projectId: '7wv7ib2t',
-    dataset: 'production'
-  }
-})
---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Landing from './components/Landing/Landing'
+import { Route, Routes } from 'react-router-dom'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <Routes>
+      <Route path='/' element={<Landing/>}/>
+    </Routes>
+    
+    </>
+  )
+}
+
+export default App
