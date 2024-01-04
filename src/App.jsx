ce8031a675c7a555e2c9877f6c6f5fc901385de7
Mercy Nyambura Kariuki <kariuki.mercie@gmail.com>
2024-01-04 09:05:20 +0000

    file transfer

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-import './App.css'
-import Landing from './components/Landing'
+// import React from 'react'
 import { Route, Routes } from 'react-router-dom'
-import DS from './components/DS'
-import RD from './components/RD'
-import Resume from './components/Resume'
+import Home from './components/Home/Home'
+import About from './components/About/About'
+import Projects from './components/Projects/Projects'
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
     <Routes>
-      <Route path='/' element={<Landing/>}/>
-      <Route path='/data-science-portfolio' element={<DS/>}/>
-      <Route path='/react-developer-portfolio' element={<RD/>}/>
-      <Route path='/resume' element={<Resume/>}/>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/about' element={<About/>}/>
+      <Route path='/projects' element={<Projects/>}/>
     </Routes>
-    
     </>
   )
 }
